2f8a9747d1d3c7a8a12ecc90c36c4c627c6003f9
sunridden <kevinszeto9449@gmail.com>
2024-10-28 11:41:01 -0700

    Constrain player position to screen

--- player.gd
+++ player.gd
@@ -4,6 +4,9 @@ const SPEED = 300.0
 
 func _physics_process(delta: float) -> void:
 	
+	var screen_size = get_viewport_rect().size
+	var player_size = $CollisionShape2D.shape.get_rect().size
+	
 	#Move player left, right, up, or down
 	var horitzontalDirection := Input.get_axis("ui_left", "ui_right")
 	var verticalDirection := Input.get_axis("ui_up", "ui_down")
@@ -27,5 +30,9 @@ func _physics_process(delta: float) -> void:
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * SPEED
 		$AnimatedSprite2D.play()
-		
+	
+	# Constrain player position to screen
+	position.x = clamp(position.x, 25, screen_size.x - 25)
+	position.y = clamp(position.y, 40, screen_size.y - 40)
+	
 	move_and_slide()
